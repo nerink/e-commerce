576bbfe73898ffb2c15afcda153bd37cd8bc035d
nkaur <nerinkaur2@gmail.com>
2021-09-18 19:01:09 -0400

    updated files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,7 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category- category can have multipl eproducts, but a product can only belong to one category 
 
 // Categories have many Products
 
