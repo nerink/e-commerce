c6cab383e492776aaf61e027ede85c6185a0519f
nkaur <nerinkaur2@gmail.com>
2021-09-18 21:49:39 -0400

    updated files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,45 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category- category can have multipl eproducts, but a product can only belong to one category 
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag_id'
+})
+
+//Make sure you set up foreign key relationships that match the column we created in the respective models.
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+})
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+})
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+})
+
 
 module.exports = {
   Product,
